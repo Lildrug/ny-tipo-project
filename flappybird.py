312e16e709edc781b7ff694442aaf7ac4159c53e
lilfrug <lildrughill.2112@gmail.com>
2023-03-01 17:35:24 +0500

    VERU POCHTI

--- flappybird.py
+++ flappybird.py
@@ -60,7 +60,7 @@ move = jump_force+1
 FPS = 60
 clock = pygame.time.Clock()
 schore=0
-b=1
+b=0
 while True:
     kolonna5_rect.x-=5
     kolonna_rect.x-=5
@@ -96,7 +96,7 @@ while True:
         kolonna2_rect.x=1500
         kolonna5_rect.x=1500
         korda2=random.randint(-690,0)
-        korda5=korda2+1000
+        korda5=korda2+1000  
         kolonna2_rect.y=korda2
         kolonna5_rect.y=korda5
     if kolonna3_rect.x<=-100:
@@ -108,7 +108,9 @@ while True:
         kolonna6_rect.y=korda6
     if kolonna_rect.x==0 or kolonna2_rect.x==0 or kolonna3_rect.x==0:
         schore=schore+1
-    if a==1 and ((bird_rect.bottom==grass_rect.top or bird_rect.top==sc_rect.top) or kolonna_rect.collidepoint(bird_rect.topright) or kolonna2_rect.collidepoint(bird_rect.topright) or kolonna3_rect.collidepoint(bird_rect.topright) or kolonna4_rect.collidepoint(bird_rect.topright)  or kolonna5_rect.collidepoint(bird_rect.topright) or kolonna6_rect.collidepoint(bird_rect.topright)):
+    if a==1 and ((bird_rect.bottom==grass_rect.top or bird_rect.top==sc_rect.top) or kolonna_rect.colliderect(bird_rect) or kolonna2_rect.colliderect(bird_rect) or kolonna3_rect.colliderect(bird_rect) or kolonna4_rect.colliderect(bird_rect)  or kolonna5_rect.colliderect(bird_rect) or kolonna6_rect.colliderect(bird_rect)):
+        b=1
+    if b==1:
         sc.fill(BLACK)
         text2 = f2.render('GOOD GAME BRO', 1, (180, 0, 0))
         text3=f3.render('Хочешь сыграть вновь? 1-Да 2-Нет',1,(180,0,0))
@@ -121,6 +123,7 @@ while True:
         if keys[49]:
             schore=0
             a=0
+            b=0
             kolonna_rect.x=900
             kolonna4_rect.x=900
             kolonna5_rect.x=1500
@@ -129,36 +132,16 @@ while True:
             kolonna6_rect.x=2100
         if keys[50]:
             break
-    if a==1 and ((bird_rect.bottom==grass_rect.top or bird_rect.top==sc_rect.top) or kolonna_rect.collidepoint(bird_rect.bottomleft) or kolonna2_rect.collidepoint(bird_rect.bottomleft) or kolonna3_rect.collidepoint(bird_rect.bottomleft) or kolonna4_rect.collidepoint(bird_rect.bottomleft)  or kolonna5_rect.collidepoint(bird_rect.bottomleft) or kolonna6_rect.collidepoint(bird_rect.bottomleft)):
-        sc.fill(BLACK)
-        text2 = f2.render('GOOD GAME BRO', 1, (180, 0, 0))
-        text3=f3.render('Хочешь сыграть вновь? 1-Да 2-Нет',1,(180,0,0))
-        sc.blit(zxc,((W//2)-250,(H//2)-250))
-        sc.blit(text2, ((W//2)-100, 900))
-        sc.blit(text1,((W//2)-50, 800))
-        sc.blit(text3,(400,100))
-        pygame.display.update()
-        keys = pygame.key.get_pressed()
-        if keys[49]:
-            schore=0
-            a=0
-            kolonna_rect.x=900
-            kolonna4_rect.x=900
-            kolonna5_rect.x=1500
-            kolonna2_rect.x=1500
-            kolonna3_rect.x=2100
-            kolonna6_rect.x=2100
-        if keys[50]:
-            break
-    sc.fill(BLUE)
-    sc.blit(kolonna,(kolonna_rect))
-    sc.blit(kolonna2,(kolonna2_rect))
-    sc.blit(kolonna3,(kolonna3_rect))
-    sc.blit(kolonna4,(kolonna4_rect))
-    sc.blit(kolonna5,(kolonna5_rect))
-    sc.blit(kolonna6,(kolonna6_rect))
-    sc.blit(text1, (10, 10))
-    sc.blit(bird, bird_rect)
-    sc.blit(grass,grass_rect)
-    clock.tick(FPS)
-    pygame.display.update() 
+    else:
+        sc.fill(BLUE)
+        sc.blit(kolonna,(kolonna_rect))
+        sc.blit(kolonna2,(kolonna2_rect))
+        sc.blit(kolonna3,(kolonna3_rect))
+        sc.blit(kolonna4,(kolonna4_rect))
+        sc.blit(kolonna5,(kolonna5_rect))
+        sc.blit(kolonna6,(kolonna6_rect))
+        sc.blit(text1, (10, 10))
+        sc.blit(bird, bird_rect)
+        sc.blit(grass,grass_rect)
+        clock.tick(FPS)
+        pygame.display.update() 
